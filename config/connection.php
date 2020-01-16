16103ef7067106d4fdfb4bc5dce7776495b7af44
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-01-16 19:25:26 +0700

    UPDATE: database

--- config/connection.php
+++ config/connection.php
@@ -1,5 +1,5 @@
 <?php
-w = mysqli_connect("localhost", "root", "", "dcs_kost");
+$connection = mysqli_connect("localhost", "root", "", "dss_kost");
 if (mysqli_connect_errno()) {
     echo "Koneksi database gagal : " . mysqli_connect_error();
 }
